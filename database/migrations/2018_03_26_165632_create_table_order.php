e50f507b3ef9c1398713d8003d5c3e6303a27a99
Mona Mahmoud <mona.mahmoud00@gmail.com>
2018-03-26 19:22:27 +0200

    DEVELOP: Create Order and Order items migrations files

---
+++ database/migrations/2018_03_26_165632_create_table_order.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableOrder extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('order_id');
+            $table->string('email');
+            $table->decimal('total_amount_net', 8, 2);
+            $table->decimal('shipping_costs', 8, 2);
+            $table->string('payment_method');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('orders');
+    }
+}
---
+++ database/migrations/2018_03_26_171742_create_table_order_items.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableOrderItems extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('order_id');
+            $table->string('name');
+            $table->integer('qnt');
+            $table->decimal('value', 8, 2);
+            $table->string('category');
+            $table->string('subcategory');
+            $table->integer('collection_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('order_items');
+    }
+}
